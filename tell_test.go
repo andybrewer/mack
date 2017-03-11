14eb96f96d942037416bc2a90d72155f768f332a
Gareth Watts <gareth@anki.com>
2017-03-11 15:02:09 -0800

    Fix broken unit test for Tell

--- tell_test.go
+++ tell_test.go
@@ -1,17 +1,23 @@
 /*
 ** Mack: Tell Test
 ** Test tell application
-*/
+ */
 
 package mack
 
 import "testing"
 
 func TestTell(t *testing.T) {
+	tell := func(application string, commands ...string) error {
+		_, err := Tell(application, commands...)
+		return err
+	}
+
 	validCommands := []ErrorAssert{
-		ErrorAssert{actual: Tell("TextEdit", "activate")},
-		ErrorAssert{actual: Tell("TextEdit", "quit")},
-		ErrorAssert{actual: Tell("Finder",
+		ErrorAssert{actual: tell("TextEdit", "activate")},
+		ErrorAssert{actual: tell("TextEdit", "activate")},
+		ErrorAssert{actual: tell("TextEdit", "quit")},
+		ErrorAssert{actual: tell("Finder",
 			"activate",
 			`open (POSIX file "/Applications")`)},
 	}
